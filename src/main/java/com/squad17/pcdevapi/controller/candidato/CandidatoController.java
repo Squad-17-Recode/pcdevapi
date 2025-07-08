b91145ca27484c8fad514bdecae09c3e9d4b2ddb
Matheus Moreira <mthsmoreira59@gmail.com>
2025-07-08 10:37:10 -0300

    Remove unused imports from CandidatoController

--- src/main/java/com/squad17/pcdevapi/controller/candidato/CandidatoController.java
+++ src/main/java/com/squad17/pcdevapi/controller/candidato/CandidatoController.java
@@ -1,9 +1,7 @@
 package com.squad17.pcdevapi.controller.candidato;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
